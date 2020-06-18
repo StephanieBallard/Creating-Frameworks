66436b79abcec88fd8d5561eab14ce98b9c9d56b
Stephanie <user.name>
2020-06-18 15:44:36 -0700

    .

---
+++ LoadingUIFrameworkDevelopment/LoadingUIFrameworkDevelopment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoadingUIFrameworkDevelopment
+//
+//  Created by Stephanie Ballard on 6/18/20.
+//  Copyright © 2020 Stephanie Ballard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
